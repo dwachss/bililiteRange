b63fdedd4bf851b4ceb7ba58d02e5504f3fc5381
dwachss <d.wachss@prodigy.net>
2013-12-18 15:27:21 -0600

    New status.js to use Promises

--- jquery.status.js
+++ jquery.status.js
@@ -1,6 +1,6 @@
 // create a "status bar" to display messages and accept line input
 
-// version 1.0
+// version 1.1
 // Documentation at http://bililite.com/blog/2013/12/11/new-jquery-plugin-statusbar/
 
 // Copyright (c) 2013 Daniel Wachsstock
@@ -31,7 +31,7 @@
 var defaults = {
 	show: $.fn.show,
 	hide: function() {return this.fadeOut(5000)},
-	result: undefined,
+	returnPromise: false,
 	run: $.noop, // function to which to pass the text to resolve result
 	prompt: false,
 	successClass: 'success',
@@ -51,47 +51,44 @@ $.fn.status = function(message, classname, opts){
 
 	opts = $.extend({}, defaults, opts)
 	var self = this;
+	function show($el) {opts.show.call($el)};
+	function hide($el) {opts.hide.call($el); $el.promise().done(function() {$el.remove()})};
 
 	if (message) return this.each(function(){
 		// just show the message
 		var span = $('<span>').addClass(classname).text(message).hide().appendTo(this);
-		opts.show.call(span);
-		opts.hide.call(span);
-		span.promise().done(function() {span.remove()});
+		show(span);
+		hide(span);
 	});
 	
-	if (!opts.result) opts.result = $.Deferred();
-
-	function resolve (text){
-		try{
-			opts.result.resolve({message: opts.run(text)});
-		}catch(e){
-			opts.result.reject({message: e.message});
+	var result = new Promise(function(resolve, reject){
+		if (opts.prompt === false){
+			// no input needed
+			resolve(opts.run());
+			return;
 		}
-	}
-		
-	opts.result.done(function(data) {
-		if (data.message) self.status(data.message, opts.successClass, opts);
-	}).fail(function(data){
-		if (data.message) self.status(data.message, opts.failureClass, opts);
-	});
-		
-	if (opts.result.state() != 'pending') return; // nothing more to do; the result has already been resolved
-		
-	if (opts.prompt === false) resolve(); // no input needed
-		
-	if (opts.result.state() == 'pending'){
-		// we need to actually do something!
-		var input = createinput(this, opts); // this actually returns the label that contains the input
-		var history = this.data('statusbar.history') || []; // a stack of past commands
-		this.data('statusbar.history', history);
+
+		// insert a <label>Prompt <input /></label>
+		var oldtext = $('label input', self).val(); // use the old text if it exists
+		$('label', self).remove(); // remove any old elements
+		var input = $('<label>').hide().text(opts.prompt).prependTo(self);
+		$('<input>').appendTo(input).val(oldtext);
+		show(input);
+
+		var history = self.data('statusbar.history') || []; // a stack of past commands
+		self.data('statusbar.history', history);
+
 		$('input',input).on('keyup', function (evt){
 			if (evt.which == 13){ // enter
 				history.push(this.value);
-				resolve (this.value);
+				// Promises fail here! The event handler catches the error before the Promise handler does!
+				try {	resolve (opts.run(this.value)) }
+				catch (e) { reject(e) }
+				hide(input);
 				return false;
 			}else if (evt.which == 27){ // esc
-				opts.result.reject({message: opts.cancelMessage});
+				reject(new Error(opts.cancelMessage));
+				hide(input);
 				return false;
 			}else if (evt.which == 38){ // up arrow
 				this.value = history.pop();
@@ -100,26 +97,18 @@ $.fn.status = function(message, classname, opts){
 		}).on('keypress', function (evt){
 			if (evt.which == 13) evt.preventDefault(); // don't pass the return to enclosing forms
 		});
-		$('input',input)[0].focus();
-	}
-	return this; // chain
+		$('input',input)[0].focus(); // focus the input box so 		
+	});
+	
+	result.then(function(message) {
+		if (message) self.status(message, opts.successClass, opts);
+	},function(err){
+		if (err.message) self.status(err.message, opts.failureClass, opts);
+	});
+	
+	return opts.returnPromise ? result : this; // chain
 };
 
 $.fn.status.defaults = defaults; // expose defaults
 
-
-function createinput(container, options){
-	// inserts a <label>Prompt: <input/></label> in container
-	var oldtext = $('label input', container).val(); // use the old text if it exists
-	$('label', container).remove(); // remove any old elements
-	var inputlabel = $('<label>').hide().text(options.prompt).prependTo(container);
-	$('<input>').appendTo(inputlabel).val(oldtext);
-	options.result.always(function(){
-		// when we're done, get rid of the element
-		options.hide.call(inputlabel);
-		inputlabel.promise().done(function() {inputlabel.remove()});
-	});
-	options.show.call(inputlabel);
-	return inputlabel;
-}
 })(jQuery);
