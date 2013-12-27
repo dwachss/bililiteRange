014af4583aef97262a3b50c1a3a38076965a22fd
dwachss <d.wachss@prodigy.net>
2013-12-27 03:11:35 -0600

    add debouncing to prismeditor

--- prismeditor.js
+++ prismeditor.js
@@ -1,7 +1,8 @@
 // Quick syntax-highlighting editor that uses Prism
-// usage: Prism.editor(pre-element). the <pre> should have the appropriate class=language-* for Prism.
+// usage: Prism.editor(element). the element should have the appropriate class=language-* for Prism.
 // depends on bililiteRange
-// Version: 1.1
+// Version: 1.2
+// Documentation: http://bililite.com/blog/2013/12/16/simple-syntax-highlighting-editor-with-prism/
 // Copyright (c) 2013 Daniel Wachsstock
 // MIT license:
 // Permission is hereby granted, free of charge, to any person
@@ -25,7 +26,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
-Prism.editor = function(editor){
+Prism.editor = function(editor, threshold){
 	if (editor.tagName.toLowerCase() == 'textarea'){
 		// turn the editor into an editable <pre>, since that is what Prism works on
 		var replacement = document.createElement('pre');
@@ -38,6 +39,21 @@ Prism.editor = function(editor){
 		editor = replacement;
 	}
 
+	// for large texts, it can be too slow to run the highlighter on every input event. 
+	// use the code from http://unscriptable.com/2009/03/20/debouncing-javascript-methods/
+	// to limit it to once every threshold milliseconds
+	function debounce (func, threshold){
+		if (!threshold) return func; // no debouncing
+		var timeout;
+		return function(){
+			var self = this, args = arguments;
+			clearTimeout(timeout);
+			timeout = setTimeout(function(){
+				func.apply(self, args);
+			}, threshold);
+		};
+	}
+	
 	var rng = bililiteRange(editor);
 	function highlight(){
 		rng.bounds('selection');
@@ -48,7 +64,7 @@ Prism.editor = function(editor){
 		rng.select();
 	}
 	highlight();
-	editor.addEventListener('input', highlight);
+	editor.addEventListener('input', debounce(highlight, threshold));
 	editor.addEventListener('paste', function(evt){
 		// Firefox changes newlines to br's on paste!
 		// Chrome pastes cr's! Nothing is easy.
