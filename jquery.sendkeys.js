adf3a9b871a2d5b6a4c145e68d1a625993fe1bf0
Danny Wachsstock <d.wachss@prodigy.net>
2020-07-14 20:54:53 -0500

    jquery.sendkeys.js: update for modern browsers

--- jquery.sendkeys.js
+++ jquery.sendkeys.js
@@ -1,34 +1,6 @@
-// insert characters in a textarea or text input field
-// special characters are enclosed in {}; use {{} for the { character itself
-// documentation: http://bililite.com/blog/2008/08/20/the-fnsendkeys-plugin/
-// Version: 4
-// Copyright (c) 2013 Daniel Wachsstock
-// MIT license:
-// Permission is hereby granted, free of charge, to any person
-// obtaining a copy of this software and associated documentation
-// files (the "Software"), to deal in the Software without
-// restriction, including without limitation the rights to use,
-// copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following
-// conditions:
-
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
-
 (function($){
 
 $.fn.sendkeys = function (x){
-	x = x.replace(/([^{])\n/g, '$1{enter}'); // turn line feeds into explicit break insertions, but not if escaped
 	return this.each( function(){
 		bililiteRange(this).bounds('selection').sendkeys(x).select();
 		this.focus();
@@ -41,8 +13,8 @@ $.fn.sendkeys = function (x){
 $.event.special.keydown = $.event.special.keydown || {};
 $.event.special.keydown._default = function (evt){
 	if (evt.isTrusted) return false;
-	if (evt.ctrlKey || evt.altKey || evt.metaKey) return false; // only deal with printable characters. This may be a false assumption
 	if (evt.key == null) return false; // nothing to print. Use the keymap plugin to set this 
+	if (evt.ctrlKey || evt.altKey || evt.metaKey) return false; // only deal with printable characters.
 	var target = evt.target;
 	if (target.isContentEditable || target.nodeName == 'INPUT' || target.nodeName == 'TEXTAREA') {
 		// only insert into editable elements
